7097bf11fd3f377776aac236ff71ca9b835190f3
peng.yu <peng.yu@qjfu.cn>
2018-08-07 10:53:42 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,9 @@ dist=$1
 if [ ! -n "$dist" ]; then
     echo 'usage : ./install.sh ~'
 elif [ -d $dist ]; then
-    config=".vimrc"
-    for i in $config; do
-        ln -sf `pwd`/$i $dist
-    done
-    dist_vim="$dist/.vim"
-    if [ ! -d $dist_vim ]; then
-        ln -sf `pwd` $dist_vim
+    ln -sf $PWD/.vimrc $dist
+    if [ ! -d $dist/.vim ]; then
+        ln -sf $PWD $dist
     fi
 else
     echo 'not found dir! :'$dist
